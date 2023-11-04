b87259dd3ce787cf906d2c27c65cc3b2dd2412f8
Franz Lorenz <freelancer_franzlorenz@web.de>
2023-11-04 18:01:53 +0100

    First draft

---
+++ serviceworker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'Calculator';
+const assetsToCache = [
+    './',
+    './icon_48.png',
+    './icon_144.png',
+    './icon_192.png',
+    './icon_512.png',
+    './manifest.json',
+    './index.html',
+    './framework7-bundle.min.css',
+    './framework7-bundle.min.js',
+    './framework7-bundle.min.js.map',
+    './navbaricon.svg',
+    './navbariconr.svg'
+];
+
+self.addEventListener( 'install', function( event ) 
+{   event.waitUntil( caches.open(CACHE_NAME)
+      .then( function(cache) 
+      {  return cache.addAll( assetsToCache ); } )
+   );
+} );
+
+self.addEventListener('fetch', function(event) 
+{  event.respondWith( caches.match(event.request)
+      .then( function(response) 
+      {  return response || fetch(event.request); } )
+   );
+} );
