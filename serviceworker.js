65076cc50ea7047103fc1e92bfdaaa96bea68219
Franz Lorenz <freelancer_franzlorenz@web.de>
2023-11-27 21:13:32 +0100

    Update Serviceworker
    Make app offline
    Add function hex()
    Add function bin()

--- serviceworker.js
+++ serviceworker.js
@@ -1,29 +1,39 @@
 const CACHE_NAME = 'Calculator';
-const assetsToCache = [
-    './',
-    './icon_48.png',
-    './icon_144.png',
-    './icon_192.png',
-    './icon_512.png',
-    './manifest.json',
-    './index.html',
-    './framework7-bundle.min.css',
-    './framework7-bundle.min.js',
-    './framework7-bundle.min.js.map',
-    './navbaricon.svg',
-    './navbariconr.svg'
+const contentToCache = [
+   './',
+   './favicon.ico',
+   './favicon.svg',
+   './framework7-bundle.min.css',
+   './framework7-bundle.min.js',
+   './framework7-bundle.min.js.map',
+   './icon_48.png',
+   './icon_144.png',
+   './icon_192.png',
+   './icon_512.png',
+   './index.css',
+   './index.html',
+   './manifest.json',
+   './navbaricon.svg',
+   './navbariconr.svg',
+   './serviceworker.js'
 ];
 
 self.addEventListener( 'install', function( event ) 
-{   event.waitUntil( caches.open(CACHE_NAME)
-      .then( function(cache) 
-      {  return cache.addAll( assetsToCache ); } )
+{  event.waitUntil( caches.open( CACHE_NAME )
+      .then( function( cache ) 
+      {  return cache.addAll( contentToCache ); } )
    );
 } );
 
-self.addEventListener('fetch', function(event) 
-{  event.respondWith( caches.match(event.request)
-      .then( function(response) 
-      {  return response || fetch(event.request); } )
-   );
-} );
+self.addEventListener( "fetch", ( event ) => 
+{
+   event.respondWith( ( async () => 
+   {
+      const r = await caches.match( event.request );
+      console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
+      if( r ) { return r; }
+      const response = await fetch( event.request );
+      return response;
+   } )(), );
+ } );
+ 
